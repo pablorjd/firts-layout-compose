8e5a08655a168c41b84297cf644ee94b1e574cee
pablorjd1 <pablo.jimenez2221@gmail.com>
2024-01-07 09:26:20 -0300

    firts-commit

---
+++ app/src/main/java/space/pablorjd/layouts_compose/MainActivity.kt
@@ -0,0 +1,173 @@
+package space.pablorjd.layouts_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import space.pablorjd.layouts_compose.ui.theme.LayoutscomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutscomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyRow()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyComplexLayout() {
+    Column(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Cyan), contentAlignment = Alignment.Center
+        ) {
+
+            Text(text = "Ejemplo 1")
+
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Red), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 2")
+            }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Green), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 3")
+            }
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Magenta), contentAlignment = Alignment.BottomCenter
+        ) {
+
+            Text(text = "Ejemplo 2")
+
+        }
+    }
+}
+
+@Composable
+fun MyRow() {
+//    Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween) {
+//        Text(text = "Ejemplo")
+//        Text(text = "Ejemplo")
+//        Text(text = "Ejemplo")
+//        Text(text = "Ejemplo")
+//    }
+
+    Row(
+        Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo", modifier = Modifier.width(100.dp))
+
+    }
+
+}
+
+@Composable
+fun MyColumn() {
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
+        Text(
+            text = "Text 13",
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+        Text(
+            text = "Text 13",
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+        Text(
+            text = "Text 13",
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+    }
+
+}
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(230.dp)
+                .height(230.dp)
+                .background(Color.Cyan)
+                .verticalScroll(
+                    rememberScrollState()
+                ), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Esto es un ejemplo")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LayoutscomposeTheme {
+        MyComplexLayout()
+    }
+}
